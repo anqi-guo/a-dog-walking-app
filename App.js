26ad43a60bc8b764c1e850e09561ddb3814066b6
Anqi Guo <guo.anqi@northeastern.edu>
2024-04-25 13:27:24 -0700

    create navigator

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
