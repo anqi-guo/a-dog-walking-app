171e45bef1a9eff7fca2666de563029bf25ca974
Anqi Guo <guo.anqi@northeastern.edu>
2024-04-25 13:31:26 -0700

    display map

---
+++ components/Home.js
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import MapView from 'react-native-maps'
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+})
