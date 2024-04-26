67585e9e9d595079bf4596395670e9a6af0fb05b
Anqi Guo <guo.anqi@northeastern.edu>
2024-04-26 15:01:44 -0700

    moved navigation related functions to Navigator.js

--- App.js
+++ App.js
@@ -1,50 +1,7 @@
 import 'react-native-gesture-handler';
-import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
-import { NavigationContainer, DrawerActions } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Entypo, MaterialIcons } from '@expo/vector-icons';
-import Walk from './components/Walk';
-import Monitor from './components/Monitor';
-import WalkHistory from './components/WalkHistory';
-import Profile from './components/Profile';
-
-const Drawer = createDrawerNavigator();
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function StackScreen() {
-  return (
-    <Stack.Navigator screenOptions={{headerShown:false}}>
-      <Stack.Screen name="Home" component={TabScreen} />
-      <Stack.Screen name="Walk" component={Walk} />
-      <Stack.Screen name="Monitor" component={Monitor} />
-    </Stack.Navigator>
-  );
-}
-
-function TabScreen() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Walks" component={WalkHistory} 
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Entypo name="list" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tab.Screen name="Profile" component={Profile} 
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialIcons name="account-circle" size={size} color={color} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-}
-
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { TabScreen } from './components/Navigator';
 
 export default function App() {
   return (
---
+++ components/Navigator.js
@@ -0,0 +1,56 @@
+import 'react-native-gesture-handler';
+import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
+import { NavigationContainer, DrawerActions } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Entypo, MaterialIcons } from '@expo/vector-icons';
+import Walk from './Walk';
+import Monitor from './Monitor';
+import WalkHistory from './WalkHistory';
+import Profile from './Profile';
+
+const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function StackScreen() {
+  return (
+    <Stack.Navigator screenOptions={{headerShown:false}}>
+      <Stack.Screen name="Home" component={TabScreen} />
+      <Stack.Screen name="Walk" component={Walk} />
+      <Stack.Screen name="Monitor" component={Monitor} />
+    </Stack.Navigator>
+  );
+}
+
+export function TabScreen() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Walks" component={WalkHistory} 
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen name="Profile" component={Profile} 
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="account-circle" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
