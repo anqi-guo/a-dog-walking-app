9f4eeeb49d4eb0c64fd0f34465240def7e5f29a9
Anqi Guo <guo.anqi@northeastern.edu>
2024-04-26 02:15:43 -0700

    list all walks

--- components/WalkHistory.js
+++ components/WalkHistory.js
@@ -1,12 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { onSnapshot, collection } from 'firebase/firestore';
+import { db } from '../firebase-files/firebaseSetup';
 
 export default function WalkHistory() {
+  const [ walks, setWalks ] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, 'walks'), (snapshot) => {
+      const walks = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setWalks(walks);
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <View>
-      <Text>WalkHistory</Text>
+      <FlatList
+        data={walks}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+            <Text>{item.id}</Text>
+            {/* <Text>{item.date}</Text>
+            <Text>{item.duration} minutes</Text>
+            <Text>{item.distance} km</Text> */}
+          </View>
+        )}
+      />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  itemContainer: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+})
