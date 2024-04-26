fb9287d012c3ae8d0cbbcfaee24727ccfaf784c1
Anqi Guo <guo.anqi@northeastern.edu>
2024-04-26 02:08:31 -0700

    add home icon on top right

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import 'react-native-gesture-handler';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Entypo } from '@expo/vector-icons';
 import Home from './components/Home';
 import Monitor from './components/Monitor';
 import WalkHistory from './components/WalkHistory';
@@ -12,7 +13,7 @@ const HomeStack = createNativeStackNavigator();
 
 function HomeStackScreen() {
   return (
-    <HomeStack.Navigator screenOptions={{headerShown: false}}>
+    <HomeStack.Navigator screenOptions={{headerShown:false}}>
       <HomeStack.Screen name="Home" component={Home} />
       <HomeStack.Screen name="Monitor" component={Monitor} />
     </HomeStack.Navigator>
@@ -22,7 +23,15 @@ function HomeStackScreen() {
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
+      <Drawer.Navigator screenOptions={({ navigation }) => ({
+        headerRight: () => (
+          <TouchableOpacity onPress={
+            () => navigation.navigate('HomeStack', { screen: 'Home' })
+          }>
+            <Entypo name="home" size={22} color="black" style={{marginRight: 10}}/>
+          </TouchableOpacity>
+        ),
+      })}>
         <Drawer.Screen name="HomeStack" component={HomeStackScreen} options={{title: "Walk"}}/>
         <Drawer.Screen name="WalkHistory" component={WalkHistory} options={{title: "Walk History"}}/>
       </Drawer.Navigator>
