ce68b73659f849a6f6a3cf8e949dc788941c9727
Anqi Guo <guo.anqi@northeastern.edu>
2024-04-27 01:44:50 -0700

    add poops and pees to WalkContext

--- components/WalkContext.js
+++ components/WalkContext.js
@@ -6,9 +6,17 @@ export function WalkProvider({ children }) {
   const [positions, setPositions] = useState([]);
   const [currentLocation, setCurrentLocation] = useState(null);
   const [isTracking, setIsTracking] = useState(false);
+  const [pees, setPees] = useState([]);
+  const [poops, setPoops] = useState([]);
 
   return (
-    <WalkContext.Provider value={{ positions, setPositions, currentLocation, setCurrentLocation, isTracking, setIsTracking }}>
+    <WalkContext.Provider value={{ 
+      positions, setPositions, 
+      currentLocation, setCurrentLocation, 
+      isTracking, setIsTracking,
+      pees, setPees,
+      poops, setPoops,
+    }}>
       {children}
     </WalkContext.Provider>
   )
